16f2d82e5144cc5766958c862c0cb43373d4d5df
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-09 11:54:07 -0700

    Added functionality that recent chats are above and oldest are below

--- SkyChat/src/components/main/Main.tsx
+++ SkyChat/src/components/main/Main.tsx
@@ -7,7 +7,8 @@ interface User {
     name: string,
     message: string,
     avatarUrl?: string,
-    whenTexted: number
+    whenTexted: number,
+    id: number
 }
 
 export default function Main() {
@@ -15,11 +16,26 @@ export default function Main() {
     const [newChats, setNewChats] = useState<Array<User>>([])
     const [oldChats, setOldChats] = useState<Array<User>>([])
 
-    if (users.length > 0) {
-        if (users.length > 3) {
-            
+    // Adding users into arrays
+    if (users.length > 0 && newChats.length <= 0) {
+        const newChatsTemp: Array<User> = []
+        const oldChatsTemp: Array<User> = []
+
+        for (let i: number = 0; i < users.length; i++) {
+            if (i > 2) {
+                oldChatsTemp.push(users[i])
+            } else {
+                newChatsTemp.push(users[i])
+            }
+
         }
+
+        setNewChats(newChatsTemp)
+        setOldChats(oldChatsTemp)
     }
+
+    console.log("New Chats: " + newChats)
+    console.log("Old Chats: " + oldChats)
 
     // Provides text if there are or are not chats
     function newChatsBlock(): JSX.Element {
@@ -29,7 +45,7 @@ export default function Main() {
             return <ul>{
 
                 newChats.map(chat => {
-                    return (<li>
+                    return (<li key={chat.id}>
                         <span>{chat.name}</span>
                         <p>{chat.message}</p>
                     </li>)
@@ -41,7 +57,7 @@ export default function Main() {
 
     // Provides oldest chats from the oldChats array
     function oldChatsBlock(): JSX.Element | null {
-        if (oldChatsBlock.length > 0) {
+        if (oldChats.length > 0) {
             return (
                 <>
                     <div>Oldest</div>
@@ -49,8 +65,8 @@ export default function Main() {
                     <section>
                         <ul>
                             {oldChats.map(chat => {
-                                return (<li>
-                                    <span>chat.name</span>
+                                return (<li key={chat.id}>
+                                    <span>{chat.name}</span>
                                     <p>{chat.message}</p>
                                 </li>)
                             })}
--- SkyChat/src/components/main/users.ts
+++ SkyChat/src/components/main/users.ts
@@ -2,24 +2,28 @@ export default [
     {
         name: "Aleksandr Ershov",
         message: "I should have texted here something exciting, but not this time I guess...",
-        whenTexted: 4
+        whenTexted: 4,
+        id: 1
     },
     
     {
         name: "Someone BlaBlavich",
         message: "I should have texted here something exciting, but not this time I guess...",
-        whenTexted: 10
+        whenTexted: 10,
+        id: 2
     },
 
     {
         name: "Hop Hovov",
         message: "I should have texted here something exciting, but not this time I guess...",
-        whenTexted: 10
+        whenTexted: 10,
+        id: 3
     },
 
     {
         name: "Skibidi Toilet",
         message: "I should have texted here something exciting, but not this time I guess...",
-        whenTexted: 10
+        whenTexted: 10,
+        id: 4
     }
 ]

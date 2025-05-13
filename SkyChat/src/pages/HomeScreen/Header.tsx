fbeca715dcbf36a3b0d4ae4e64794279d03fa752
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-13 10:08:30 -0700

    Added an add button and styled it

--- SkyChat/src/pages/HomeScreen/Header.tsx
+++ SkyChat/src/pages/HomeScreen/Header.tsx
@@ -1,8 +1,9 @@
 // Returns a component with an Add User Button and Messages title
 export default function Header() {
-    return <header>
-        <button></button> 
-        <h1 className="font-primary text-6xl text-center pb-15 border-b-3 
-        border-black/25 tracking-wide">Messages</h1>
+    return <header className="pb-15 border-b-3 border-black/25 relative">
+        <button className="w-15 h-15 bg-transparent rounded-2xl absolute
+        border-5 border-accent2 shadow-lg transition-all addBtnProps
+        left-18 top-0"></button> 
+        <h1 className="font-primary text-6xl text-center tracking-wide">Messages</h1>
     </header>
 }

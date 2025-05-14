42d08b33c278c222796be77580dd9c0488ced1eb
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-14 06:39:19 -0700

    Changed add button's position

--- SkyChat/src/pages/HomeScreen/Header.tsx
+++ SkyChat/src/pages/HomeScreen/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     return <header className="pb-15 border-b-3 border-black/25 relative">
         <button className="w-15 h-15 bg-transparent rounded-2xl absolute
         border-5 border-accent2 shadow-lg transition-all addBtnProps
-        left-18 top-0"></button> 
+        left-18 top-2"></button> 
         <h1 className="font-primary text-6xl text-center tracking-wide">Messages</h1>
     </header>
 }

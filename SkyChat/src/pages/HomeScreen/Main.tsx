efa795cb6b4e87f8e13be2d5e0452c05047c0176
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-16 09:27:28 -0700

    Reorganized Main component stored class props into a variable and changed interface for type

--- SkyChat/src/pages/HomeScreen/Main.tsx
+++ SkyChat/src/pages/HomeScreen/Main.tsx
@@ -2,14 +2,16 @@ import type { ReactNode } from "react"
 import Chats from "../../components/Chats"
 import users from "../../utils/users"
 
-interface MainProps {
-    children: ReactNode
+type MainProps = {
+    children: ReactNode;
 }
 
 export default function Main({ children }: MainProps) {
+    const mainClasses = `font-secondary font-normal w-full px-7.25 xl:px-30 pt-15 
+        bgwhite relative`;
 
     return (
-        <main className="font-secondary font-normal w-full px-7.25 xl:px-30 pt-15 bgwhite relative">
+        <main className={mainClasses}>
 
             {children}
 

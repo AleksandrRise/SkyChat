23dae939528d293d338e3d8c44070aa58f1e8f78
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-16 09:29:31 -0700

    Reorganized header component Stored class props into variables

--- SkyChat/src/pages/HomeScreen/Header.tsx
+++ SkyChat/src/pages/HomeScreen/Header.tsx
@@ -1,9 +1,14 @@
 // Returns a component with an Add User Button and Messages title
 export default function Header() {
-    return <header className="pb-15 border-b-3 border-black/25 relative">
-        <button className="w-15 h-15 bg-transparent rounded-2xl absolute
+    const headerClasses = `pb-15 border-b-3 border-black/25 relative`;
+    const btnClasses = `w-15 h-15 bg-transparent rounded-2xl absolute
         border-5 border-accent2 shadow-lg transition-all addBtnProps
-        left-18 top-2"></button> 
-        <h1 className="font-primary text-6xl text-center tracking-wide">Messages</h1>
+        left-18 top-2`;
+
+    const hClasses = `font-primary text-6xl text-center tracking-wide`;
+
+    return <header className={headerClasses}>
+        <button className={btnClasses}></button> 
+        <h1 className={hClasses}>Messages</h1>
     </header>
 }

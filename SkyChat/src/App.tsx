270baf35fcc92c8b88ac09184f0f3b18dcf2fe80
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-15 17:18:08 -0700

    Added a new Chat component

--- SkyChat/src/App.tsx
+++ SkyChat/src/App.tsx
@@ -1,6 +1,7 @@
 import Main from "./pages/HomeScreen/Main"
 import Aside from "./pages/HomeScreen/Aside"
 import Header from "./pages/HomeScreen/Header"
+import Chat from "./pages/HomeScreen/Chat"
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <Main>
         <Header />
       </Main>
+
+      <Chat />
+
     </section>
   )
 }

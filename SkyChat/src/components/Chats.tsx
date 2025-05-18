92b329784a17a564d4d02bbe11705149abf9b301
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-18 08:11:40 -0700

    Changed hover animation for each chat

--- SkyChat/src/components/Chats.tsx
+++ SkyChat/src/components/Chats.tsx
@@ -13,8 +13,8 @@ function ChatElement({ name, message, avatarUrl = "", whenTexted, id }: User) {
     ? avatarUrl
     : defaultPhoto
 
-    const liClasses = `w-11/12 mx-auto mt-16 flex cursor-pointer last:mb-80
-        hover:border-l-2 hover:border-black/25 hover:pl-10 transition-[padding]`;
+    const liClasses = `w-11/12 mx-auto mt-12.25 flex cursor-pointer last:mb-80
+        hover:shadow-icons p-4.25 transition`;
 
     const figClasses = `w-25 h-25 bg-gray rounded-3xl flex justify-center 
         items-center shrink-0 shadow-icons`;

f319c872c0d48d3c13200b43984d41beb23270d4
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-19 08:41:32 -0700

    Added text input design inside a Chat component

--- SkyChat/src/pages/HomeScreen/Chat.tsx
+++ SkyChat/src/pages/HomeScreen/Chat.tsx
@@ -3,10 +3,6 @@ import sendBtn from "../../assets/images/chat/sendBtn.png"
 
 export default function Chat() {
 
-    const chatClasses = `bg-gradient-to-tl from-25% from-bg-accent 
-        transition-all to-secondary border-l-1 border-black/10 h-full float-end
-        flex-6`;
-
     const messages = [
         {avatarUrl: defaultPhoto, text: "“Something discussing”", isMe: true},
         {avatarUrl: defaultPhoto, text: "Yeah... that was very exciting!", isMe: false},
@@ -16,13 +12,26 @@ export default function Chat() {
         {avatarUrl: defaultPhoto, text: "It was a pleasure to talk to you!", isMe: true},
     ]
 
+    const chatClasses = `bg-gradient-to-tl from-25% from-bg-accent 
+        transition-all to-secondary border-l-1 border-black/10 h-full float-end
+        flex-5 py-12 px-16 flex flex-col`;
+    const headClasses = "text-4xl font-primary text-center border-b-3 border-black/20 pb-5"
+    const ulClasses = "overflow-y-auto"
+    const liClasses = "flex"
+    const divClasses = "mt-auto bg-primary py-6.5 px-9 shadow-icons rounded-2xl flex w-full"
+    const sendClasses = `w-10 h-10 ml-auto cursor-pointer relative z-1 hover:before:content-[""]
+        hover:before:h-[calc(100%+1rem)] hover:before:w-[calc(100%+1rem)] hover:before:absolute hover:before:-top-2
+        hover:before:-left-3 hover:before:bg-white/25 hover:before:-z-1 hover:before:rounded-full
+        before:transition-all before:duration-300 before:ease-out`
+    const inputClasses = `text-2xl my-auto border-none outline-none w-9/10`
+
     return (
         <section className={chatClasses}>
-            <h3>Aleksandr Ershov</h3>
+            <header className={headClasses}>Aleksandr Ershov</header>
 
-            <ul>
+            <ul className={ulClasses}>
                 {messages.map((message) => (
-                    <li>
+                    <li className={liClasses}>
                         <figure>
                             <img src={message.avatarUrl} alt="Profile Photo" />
                         </figure>
@@ -36,9 +45,9 @@ export default function Chat() {
                 ))}
             </ul>
 
-            <div>
-                <input type="text" placeholder="Type a message..." />
-                <button>
+            <div className={divClasses}>
+                <input className={inputClasses} placeholder="Type a message..."/>
+                <button className={sendClasses}>
                     <figure>
                         <img src={sendBtn} alt="!Click to Send a Message!" />
                     </figure>

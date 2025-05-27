b871acf51292577cb4c3fb4a315b8a654292b0dc
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-26 21:45:03 -0700

    Added a router inside main.tsx file - root initializer

--- SkyChat/src/main.tsx
+++ SkyChat/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './assets/globals.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './assets/globals.css';
+import App from './App.tsx';
+import Main from "./pages/AuthScreen/Main.tsx";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/auth" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )

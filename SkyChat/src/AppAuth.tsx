ce13ebdc4eef82904a0905505ac3adf4619bc44a
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-26 21:57:40 -0700

    Added logo component that contains only logo

--- SkyChat/src/AppAuth.tsx
+++ SkyChat/src/AppAuth.tsx
@@ -1,7 +1,11 @@
 import Main from "./pages/AuthScreen/Main";
+import Logo from "./pages/AuthScreen/Logo.tsx";
 
 export default function AppAuth() {
     return (
-        <Main />
+        <main>
+            <Logo />
+            <Main />
+        </main>
     )
 }
---
+++ SkyChat/src/pages/AuthScreen/Logo.tsx
@@ -0,0 +1,7 @@
+import LogoImg from "../../assets/images/authpage/Logo.png";
+
+export default function Logo() {
+    return (
+        <img src={LogoImg} alt="Logo" />
+    )
+}

eb8179912fbef95698e6b152b9be5b586d270d53
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-28 22:09:58 -0700

    Fixed the issue with the border, added some transitions when focused on an input, and added some animations for the Done button

--- SkyChat/src/pages/AuthScreen/Main.tsx
+++ SkyChat/src/pages/AuthScreen/Main.tsx
@@ -1,7 +1,7 @@
 export default function Main() {
 
     // Classes Variables.
-    const formClasses = `py-18.75 px-20 bg-white color-black font-secondary
+    const formClasses = `py-18.75 px-22 bg-white color-black font-secondary
         shadow-[-4px_4px_4px_rgba(0,0,0,0.25)] rounded-sm`;
     const hClasses = "text-6xl font-primary pb-5.5";
     const pClasses = "text-xl opacity-50 pb-7";
@@ -9,11 +9,17 @@ export default function Main() {
         to-[#59A9FF] bg-clip-text text-transparent`;
     const titleSectionClasses = `text-center pb-16.5`;
     const borderClasses = `bg-linear-to-r from-transparent via-black to-transparent
-        h-0.75 w-full opacity-75`
-    const inputsSectionClasses = `pb-21 flex flex-col gap-12`
-    const inputClasses = `block outline-none w-full text-xl placeholder-black/25
-        border-1 border-b border-solid p-3.5`
-    const signinBtnClasses = `text-xl cursor-pointer underline text-[#4FA4FF]`;
+        h-0.75 w-full opacity-75`;
+    const inputsSectionClasses = `pb-21 flex flex-col gap-12`;
+    const inputClasses = `relative outline-none w-full text-xl placeholder-black/25
+        borderForInput p-3.5`;
+    const submissionClasses = `relative flex justify-center`
+    const signinBtnClasses = `text-lg cursor-pointer underline text-[#4FA4FF]
+        pb-3`;
+    const doneClasses = `text-xl px-8.5 py-3 bg-accent1 rounded-xl cursor-pointer
+        font-semibold tracking-wider absolute bottom-0 right-0 shadow-donebtn
+        hover:scale-105 duration-500 hover:duration-200 active:opacity-80
+        active:duration-0`;
 
     // Find a way to make the border only in the bottom.
     // Finish styling
@@ -40,9 +46,9 @@ export default function Main() {
                 })}
             </section>
 
-            <section>
+            <section className={submissionClasses}>
                 <button className={signinBtnClasses}>Sign In</button>
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Done!" className={doneClasses} />
             </section>
         </form>
     )

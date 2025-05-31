e12518046c10be7337022555ca6a22c0bd200f00
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-31 08:10:25 -0700

    Added Cors Configurer

--- backend/src/main/java/com/backend/main/backendController.java
+++ backend/src/main/java/com/backend/main/backendController.java
@@ -6,14 +6,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class backendController {
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @PostMapping("/greeting")
     public String sendText(@RequestBody String word) {
         System.out.println(word);
         return word;
     }
 
-    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping("/greeting")
     public String getText() {
         return "Hello!";

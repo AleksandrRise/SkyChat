a58a3864e3b0ab58c72123a65f7ab096da5e6fce
Aleksandr Ershov <aleks.usbuz@gmail.com>
2025-05-31 22:04:54 -0700

    Added basic settings like spring.datasource

--- backend/src/main/java/com/backend/main/Launcher.java
+++ backend/src/main/java/com/backend/main/Launcher.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class Launcher {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(Launcher.class, args);
 	}
 
 }
